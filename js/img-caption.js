b6355783013d3d440693dbeec53f9420f3a7033a
Saber <sasarabie@gmail.com>
2017-09-30 11:25:13 +0200

    Last
    Full Project

---
+++ js/img-caption.js
@@ -0,0 +1,17 @@
+
+	$('.work-panel').mouseenter(function(){
+    var image= $(this).find('img'),
+        caption = $(this).find('div');
+    
+    caption.width(image.width());
+    caption.height(image.height());
+    caption.fadeIn();
+}).mouseleave(function(){
+     var image= $(this).find('img'),
+        caption = $(this).find('div');
+    
+    caption.width(image.width());
+    caption.height(image.height());
+    caption.fadeOut();
+});
+
